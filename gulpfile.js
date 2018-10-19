232dfa94f793111f79a1f5f295fb3082ad158b72
Chevy Li <lzw218@zju.edu.cn>
2018-10-19 11:49:06 +0800

    add department host events page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var removeCode = require('gulp-remove-code');
 
 gulp.task('default', function() {
-    gulp.src('index.html')
+    gulp.src(['index.html', 'deptHostEvents.html'])
         .pipe(removeCode({ production: true }))
         .pipe(gulp.dest('./dist'))
 });
